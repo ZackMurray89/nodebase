6d9ebac2cc73a22f63719d7f02badc9cd693ec4a
ZackMurray89 <zackmurray.dev@gmail.com>
2025-10-15 00:33:45 -0500

    03: tRPC Setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,20 @@
-"use client";
+import { getQueryClient, trpc } from "@/trpc/server";
+import { Client } from "./client";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { Suspense } from "react";
 
-import { useTRPC } from "@/trpc/client";
-import { useQuery } from "@tanstack/react-query";
+const Page = async () => {
+  const queryClient = getQueryClient();
 
-const Page = () => {
-  const trpc = useTRPC();
-  const { data: users } = useQuery(trpc.getUsers.queryOptions());
+  void queryClient.prefetchQuery(trpc.getUsers.queryOptions());
 
   return (
     <div className="min-h-screen min-w-screen flex items-center justify-center">
-        {JSON.stringify(users)}
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <Suspense fallback={<p>Loading...</p>}>
+          <Client />
+        </Suspense>
+      </HydrationBoundary>
     </div>
   )
 }
